38fd2d83c044e5cb0e92a46d7c0bf3d40de1b1d4
Sunshiine99 <i-l-y_93@hotmail.com>
2017-02-09 20:53:09 +0000

    End of Chapter 5

--- tango_with_django_project/rango/models.py
+++ tango_with_django_project/rango/models.py
@@ -3,6 +3,8 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
+    views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
 
     class Meta:
         verbose_name_plural= 'Categories'
@@ -18,3 +20,5 @@ class Page(models.Model):
 
     def __unicode__(self):
         return self.title
+
+
